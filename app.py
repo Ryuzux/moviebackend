709e7a68d29e9b85b2faf532814a02c97b58e141
Ryuzux <renjirogunawan@gmail.com>
2024-04-09 19:13:05 +0700

    add put movie

--- app.py
+++ app.py
@@ -92,7 +92,6 @@ def add_user():
     db.session.commit()
     return jsonify({'message': 'Registered successfully'}), 201
 
-
 @app.route('/update/user/', methods=['PUT'])
 @User.admin_or_user_required
 def update_user(current_user):
@@ -106,6 +105,66 @@ def update_user(current_user):
         user.password = data['password']
     db.session.commit() 
     return jsonify({'message': 'User updated successfully'}), 200
+
+@app.route('/add/movie/', methods=['POST'])
+@User.admin_required
+def add_movie():
+    data = request.get_json()
+    existing_movie = Movie.query.filter_by(name=data['name']).first()
+    if 'name' not in data:
+        return jsonify({
+            'error': 'name movie must be input'
+        })
+    if existing_movie:
+        return jsonify({
+            'error': "the movie currently airing"
+        })
+    if 'launching' not in data:
+        return jsonify({
+            'error': 'launhcing date must be input'
+        })
+    if 'ticket_price' not in data:
+        return jsonify({
+            'error': 'ticket price must be input'
+        })
+    mov = Movie(
+        name = data['name'],
+        launching = data['launching'],
+        category_id = data['category_id'],
+        ticket_price = data['ticket_price']
+    )
+    db.session.add(mov)
+    db.session.commit()
+    return jsonify({
+        'id': mov.id,
+        'name': mov.name,
+        'launching': mov.launching.strftime('%Y-%m-%d'),
+        'category_id': mov.category_id,
+        'ticket_price': mov.ticket_price
+    })
+
+@app.route('/update/movie/', methods=['PUT'])
+@User.admin_required
+def update_movie():
+    data = request.get_json()
+    update_id = data.get('id')
+    if not update_id:
+        return jsonify({'error': 'id is required in the request'}), 400
+    movie = Movie.query.get(update_id)
+    if not movie:
+        return jsonify({'error': 'Movie not found'}), 404
+    if 'name' in data:
+        movie.name = data['name']
+    if 'launching' in data:
+        movie.launching = data['launching']
+    if 'category_id' in data:
+        movie.category_id = data['category_id']
+    if 'ticket_price' in data:
+        movie.ticket_price = data['ticket_price']
+    db.session.commit() 
+    return jsonify({'message': 'Movie updated successfully'}), 200
+
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port = 5000, debug=True)
