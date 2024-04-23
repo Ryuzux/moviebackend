7721e143bab37b02f78168cac1cec1b33287d35f
Ryuzux <renjirogunawan@gmail.com>
2024-04-23 16:46:06 +0700

    aa

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ from user import *
 from movie_manage import *
 from reporting import *
 from topup import *
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'welcome'
+
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port = 5000, debug=True)
+    app.run(debug=True)
